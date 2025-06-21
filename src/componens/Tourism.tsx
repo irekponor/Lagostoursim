cc2e21b16bd13fbaab378081eb6a13aea84e8031
emmanuel <emmanuelirekponor86@gmail.com>
2025-06-21 11:35:16 +0100

    map edits

--- src/componens/Tourism.tsx
+++ src/componens/Tourism.tsx
@@ -1,15 +1,28 @@
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
+import L from "leaflet";
 
 const Tourism = () => {
-  const [geoData, setGeoData] = useState(null);
+  const [geoData, setGeoData] = useState<any>(null);
 
   useEffect(() => {
     fetch("/Lagos.geojson")
       .then((res) => res.json())
       .then((data) => setGeoData(data));
   }, []);
+
+  const onEachFeature = (feature: any, layer: any) => {
+    const name = feature.properties?.name || "Unknown Site";
+    const description =
+      feature.properties?.description || "No description available.";
+
+    const popupContent = `
+      <strong>${name}</strong><br/>
+      <span>${description}</span>
+    `;
+    layer.bindPopup(popupContent);
+  };
 
   return (
     <MapContainer
@@ -18,7 +31,7 @@ const Tourism = () => {
       style={{ height: "100vh", width: "100%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {geoData && <GeoJSON data={geoData} />}
+      {geoData && <GeoJSON data={geoData} onEachFeature={onEachFeature} />}
     </MapContainer>
   );
 };
