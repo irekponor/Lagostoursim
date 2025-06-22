0867e3efa818b715a1da72b89bea868c6b06cc3a
emmanuel <emmanuelirekponor86@gmail.com>
2025-06-22 23:10:49 +0100

    map interactive

--- src/componens/Tourism.tsx
+++ src/componens/Tourism.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
